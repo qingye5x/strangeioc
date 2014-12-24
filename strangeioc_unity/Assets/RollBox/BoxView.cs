ed19c0f417886f6628fc7c396b142b403ee58ce3
qingye5x <ah2063@columbia.edu>
2014-12-24 17:51:10 +0800

    同步工程
    同步工程

---
+++ strangeioc_unity/Assets/RollBox/BoxView.cs
@@ -0,0 +1,23 @@
+﻿/*************************************************************************************
+    * 命名空间：       public
+    * 文 件 名：       $safeitemname$
+    * 创建时间：       $time$
+    * 作    者：       SparkSun
+    * 说   明：
+    * 修改时间：
+    * 修 改 人：
+*************************************************************************************/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxView : MonoBehaviour
+{
+    List<> 
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+}
---
+++ strangeioc_unity/Assets/RollBox/RollBox.cs
@@ -0,0 +1,31 @@
+﻿/*************************************************************************************
+    * 命名空间：       public
+    * 文 件 名：       $safeitemname$
+    * 创建时间：       $time$
+    * 作    者：       SparkSun
+    * 说   明：
+    * 修改时间：
+    * 修 改 人：
+*************************************************************************************/
+
+
+using DG.Tweening;
+using UnityEngine;
+using System.Collections;
+
+public class RollBox : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround( transform.position - new Vector3(0f,0f,0.5f) , Vector3.right,90f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.RotateAround( transform.position + new Vector3(0f, 0f, -0.5f), Vector3.right, -90f);
+        }
+    }
+
+}
